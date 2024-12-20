ad0cb99b3fe736bb111397335b448cdf221f95fa
SynclonSec <161953995+SynclonSec@users.noreply.github.com>
2024-12-20 00:21:23 -0500

    Update hyperbolic_geometry.cpp

--- src/utils/hyperbolic_geometry.cpp
+++ src/utils/hyperbolic_geometry.cpp
@@ -1,31 +1,4 @@
-#ifndef HYPERBOLIC_GEOMETRY_H
-#define HYPERBOLIC_GEOMETRY_H
-
-#include <iostream>
-#include <cmath>
-#include <stdexcept>
-#include <vector>
-#include <complex>
-#include <iomanip>
-#include <cassert>
-
-// Function declarations
-double hyperbolicSine(double x);
-double hyperbolicCosine(double x);
-double hyperbolicTangent(double x);
-double hyperbolicCotangent(double x);
-double hyperbolicSecant(double x);
-double hyperbolicCosecant(double x);
-
-std::complex<double> hyperbolicSineComplex(const std::complex<double>& z);
-std::complex<double> hyperbolicCosineComplex(const std::complex<double>& z);
-std::complex<double> hyperbolicTangentComplex(const std::complex<double>& z);
-std::complex<double> hyperbolicCotangentComplex(const std::complex<double>& z);
-std::complex<double> hyperbolicSecantComplex(const std::complex<double>& z);
-std::complex<double> hyperbolicCosecantComplex(const std::complex<double>& z);
-
-void displayComplexNumber(const std::complex<double>& number);
-void runUnitTests();
+#include "hyperbolic_geometry.h"
 
 // Hyperbolic Sine
 double hyperbolicSine(double x) {
@@ -242,5 +215,3 @@ int main() {
 
     return 0;
 }
-
-#endif // HYPERBOLIC_GEOMETRY_H
