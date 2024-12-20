d549c04df5876569bb6d9e93c60cf6ed1060fe03
SynclonSec <161953995+SynclonSec@users.noreply.github.com>
2024-12-19 23:48:32 -0500

    Update euler.h

--- src/utils/euler.h
+++ src/utils/euler.h
@@ -1,1 +1,15 @@
+#ifndef EULER_H
+#define EULER_H
 
+#include <iostream>
+#include <vector>
+#include <complex>
+#include <string>
+
+void log(const std::string& message);
+std::complex<double> eulerFormula(double x);
+std::vector<std::complex<double>> dft(const std::vector<double>& priceData);
+void displayComplexNumber(const std::complex<double>& number);
+void runUnitTests();
+
+#endif // EULER_H
